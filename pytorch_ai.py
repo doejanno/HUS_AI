a6e416ef704fb37576773d09a62601d8851c241a
luka.faensen <luka.faensen@th-bingen.de>
2024-06-18 08:00:16 +0200

    new ai framework

--- pytorch_ai.py
+++ pytorch_ai.py
@@ -1,6 +1,18 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import random
+from collections import deque
+
+class RepleayMemory:
+    def __init__(self, capacity):
+        self.memory= deque(maxlen=capacity)
+    def push(self, state, action, reward, next_state, done):
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    def __len__(self):
+        return len(self.memory)
 
 class DQN(nn.Module):
     def __init__(self):
@@ -13,3 +25,58 @@ class DQN(nn.Module):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         return self.fc3(x)
+    import torch.nn.functional as F
+
+class DQNAgent:
+    def __init__(self, state_dim, action_dim):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.epsilon = 1.0  # Exploration rate
+        self.epsilon_min = 0.1
+        self.epsilon_decay = 0.995
+        self.gamma = 0.99  # Discount factor
+        self.batch_size = 64
+        self.memory = ReplayMemory(10000)
+        self.model = DQN(state_dim, action_dim)
+        self.target_model = DQN(state_dim, action_dim)
+        self.update_target_model()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    def act(self, state):
+        if random.random() < self.epsilon:
+            return random.choice(range(self.action_dim))
+        state = torch.FloatTensor(state).unsqueeze(0)
+        with torch.no_grad():
+            q_values = self.model(state)
+        return torch.argmax(q_values).item()
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.push(state, action, reward, next_state, done)
+
+    def replay(self):
+        if len(self.memory) < self.batch_size:
+            return
+        batch = self.memory.sample(self.batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+
+        states = torch.FloatTensor(states)
+        actions = torch.LongTensor(actions)
+        rewards = torch.FloatTensor(rewards)
+        next_states = torch.FloatTensor(next_states)
+        dones = torch.FloatTensor(dones)
+
+        q_values = self.model(states).gather(1, actions.unsqueeze(1)).squeeze(1)
+        next_q_values = self.target_model(next_states).max(1)[0]
+        target_q_values = rewards + (1 - dones) * self.gamma * next_q_values
+
+        loss = F.mse_loss(q_values, target_q_values)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
