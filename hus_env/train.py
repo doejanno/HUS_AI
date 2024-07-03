b26814bc407a0717d47bc9fc4d822876e65baa0e
studfaensen <luka.faensen@th-bingen.de>
2024-07-03 22:02:33 +0200

    unbound error referenced before assignment

--- hus_env/train.py
+++ hus_env/train.py
@@ -27,47 +27,47 @@ def train_dqn(num_episodes, batch_size=32, gamma=0.99, epsilon_start=1.0, epsilo
             #print("Legal moves:", board.p1.legal_moves)
             if board.p1.is_game_over():
                 print("Game over")
-                done = true
+                done = True
                 reward = -1
-        else:	   	
-            action1 = player1.get_move(state, board.p1.legal_moves, epsilon) 
-            board.p1.move(board.p2, action1, 0)
-            reward = 0
+            else:	   	
+                action1 = player1.get_move(state, board.p1.legal_moves, epsilon) 
+                board.p1.move(board.p2, action1, 0)
+                reward = 0
 
 
-        next_state = board.get_board()
-        replay_buffer.push(state, action1, reward, next_state, done)
-        state = next_state
-        total_reward += reward
+            next_state = board.get_board()
+            replay_buffer.push(state, action1, reward, next_state, done)
+            state = next_state
+            total_reward += reward
 
-        if not done and moves < MAX_MOVES:
-            # Player 2's turn
-            #print("Player 2 turn")
-            #print("Board state:", state)
-            #print("Legal moves:", board.p2.legal_moves)
-            if board.p2.is_game_over():
-                print("Game over")
-            done = true
-            reward = -1
-        else:	   	
-            action2 = player2.get_move(state, board.p2.legal_moves, epsilon) 
-            board.p2.move(board.p1, action2, 0)
-            reward = 0
+            if not done and moves < MAX_MOVES:
+                # Player 2's turn
+                #print("Player 2 turn")
+                #print("Board state:", state)
+                #print("Legal moves:", board.p2.legal_moves)
+                if board.p2.is_game_over():
+                    print("Game over")
+                done = True
+                reward = -1
+            else:	   	
+                action2 = player2.get_move(state, board.p2.legal_moves, epsilon) 
+                board.p2.move(board.p1, action2, 0)
+                reward = 0
 
-        next_state = board.get_board()
-        replay_buffer.push(state, action2, reward, next_state, done)
-        state = next_state
-        total_reward += reward
+            next_state = board.get_board()
+            replay_buffer.push(state, action2, reward, next_state, done)
+            state = next_state
+            total_reward += reward
 
-        moves += 1  # Increment the moves counter
+            moves += 1  # Increment the moves counter
 
-        # Train both players
-        loss1 = player1.update(replay_buffer, batch_size, gamma)
-        loss2 = player2.update(replay_buffer, batch_size, gamma)
+    # Train both players
+    loss1 = player1.update(replay_buffer, batch_size, gamma)
+    loss2 = player2.update(replay_buffer, batch_size, gamma)
 
-        epsilon = max(epsilon_end, epsilon * epsilon_decay)
+    epsilon = max(epsilon_end, epsilon * epsilon_decay)
 
-        print(f"Episode {episode}, Total Reward: {total_reward}, Epsilon: {epsilon:.2f}")
+    print(f"Episode {episode}, Total Reward: {total_reward}, Epsilon: {epsilon:.2f}")
 
     return player1, player2
 
